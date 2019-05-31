7cfc4cb7f50ba33b9beeaea3a2a219f5f4f2005a
Joshua Tan <jtsarvida@gmail.com>
2019-05-31 05:10:20 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,5 +25,7 @@ class ArticlesController < ApplicationController
   end 
   
   def update 
+    @article = Article.find(params[:id])
+    @article.update(title: params[:title], description: params[:description])
   end 
 end
